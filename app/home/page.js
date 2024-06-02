c230348c9ce267c0a1891441d653b956fcc4f7b4
ashwinbhagavatula <ashwinbhagavatula02@gmail.com>
2024-06-02 22:46:59 +0530

    use effect for session check

--- app/home/page.js
+++ app/home/page.js
@@ -3,16 +3,19 @@ import Queue from '@/components/Queue'
 import { Button } from '@/components/ui/button'
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation';
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function Page() {
   const {data:session} = useSession()
   const userData = session
   const router = useRouter();
 
-  if(userData === null){
-    router.push("/")
-  }
+  useEffect(()=>{
+    if(userData === null){
+      router.push("/")
+    }
+  }, [])
+  
   return (
     <>
     <div className='mt-10 px-4 md:px-20 '>
