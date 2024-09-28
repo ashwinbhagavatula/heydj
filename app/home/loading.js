2ed0e0fd68f0e162b89222670849c700a57eaa83
ashwinbhagavatula <ashwinbhagavatula02@gmail.com>
2024-09-28 15:10:13 +0530

    added basic loading uis

---
+++ app/home/loading.js
@@ -0,0 +1,8 @@
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center h-full">
+      <p>Loading your queue...</p>
+      {/* You can also add a spinner or skeleton loading component here */}
+    </div>
+  );
+}
